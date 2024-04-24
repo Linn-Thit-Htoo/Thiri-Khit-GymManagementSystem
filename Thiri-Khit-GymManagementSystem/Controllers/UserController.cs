f9ffdf7e387f15ff7c944bae3c8ac40e8436054e
Linn Thit <linnthit77387@gmail.com>
2024-04-24 10:32:11 +0630

    [Complete] User Management

--- Thiri-Khit-GymManagementSystem/Controllers/UserController.cs
+++ Thiri-Khit-GymManagementSystem/Controllers/UserController.cs
@@ -39,5 +39,154 @@ namespace Thiri_Khit_GymManagementSystem.Controllers
                 throw new Exception(ex.Message);
             }
         }
+
+        [ActionName("UserManagement")]
+        public async Task<IActionResult> GoToUserManagement()
+        {
+            try
+            {
+                List<UserDataModel> users = await _appDbContext.Users
+                    .OrderByDescending(x => x.UserId)
+                    .ToListAsync();
+                return View(users);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        [ActionName("CreateUser")]
+        public IActionResult GoToCreateUserPage()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Save(UserDataModel userDataModel)
+        {
+            try
+            {
+                UserDataModel? user = await _appDbContext.Users
+                    .Where(x => x.Email == userDataModel.Email)
+                    .FirstOrDefaultAsync();
+
+                if (user is not null)
+                {
+                    TempData["error"] = "User with this email already exists!";
+                    return RedirectToAction("UserManagement");
+                }
+
+                userDataModel.UserRole = "member";
+                await _appDbContext.Users.AddAsync(userDataModel);
+                int result = await _appDbContext.SaveChangesAsync();
+
+                if (result > 0)
+                {
+                    TempData["success"] = "Creating Successful!";
+                }
+                else
+                {
+                    TempData["error"] = "Creating Fail!";
+                }
+                return RedirectToAction("UserManagement");
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        public async Task<IActionResult> EditUser(long id)
+        {
+            try
+            {
+                UserDataModel? user = await _appDbContext.Users
+                    .Where(x => x.UserId == id)
+                    .FirstOrDefaultAsync();
+
+                return View(user);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Update(UserDataModel userDataModel)
+        {
+            try
+            {
+
+                bool isDuplicate = await _appDbContext
+                    .Users
+                    .Where(x => x.Email == userDataModel.Email && x.UserId != userDataModel.UserId)
+                    .AnyAsync();
+                if (isDuplicate)
+                {
+                    TempData["error"] = "User with this email already exists!";
+                    return RedirectToAction("UserManagement");
+                }
+
+                UserDataModel? user = await _appDbContext.Users
+                    .Where(x => x.UserId == userDataModel.UserId)
+                    .FirstOrDefaultAsync();
+
+                if (user is not null)
+                {
+                    user.UserName = userDataModel.UserName;
+                    user.Email = userDataModel.Email;
+                    int result = await _appDbContext.SaveChangesAsync();
+                    if (result > 0)
+                    {
+                        TempData["success"] = "Updating Successful!";
+                    }
+                    else
+                    {
+                        TempData["error"] = "Updating Fail!";
+                    }
+                    return RedirectToAction("UserManagement");
+                }
+                TempData["error"] = "User not found.";
+                return RedirectToAction("UserManagement");
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        public async Task<IActionResult> Delete(long id)
+        {
+            try
+            {
+                UserDataModel? user = await _appDbContext
+                    .Users
+                    .FirstOrDefaultAsync(x => x.UserId == id);
+                if (user is null)
+                {
+                    TempData["Title"] = "User not found!";
+                    return RedirectToAction("UserManagement");
+                }
+
+                _appDbContext.Users.Remove(user);
+                int result = await _appDbContext.SaveChangesAsync();
+
+                if (result > 0)
+                {
+                    TempData["success"] = "Deleting Successful!";
+                }
+                else
+                {
+                    TempData["error"] = "Deleting Fail!";
+                }
+                return RedirectToAction("UserManagement");
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
     }
 }
--- Thiri-Khit-GymManagementSystem/Models/UserDataModel.cs
+++ Thiri-Khit-GymManagementSystem/Models/UserDataModel.cs
@@ -8,7 +8,7 @@ namespace Thiri_Khit_GymManagementSystem.Models
         public long UserId { get; set; }
         public string UserName { get; set; }
         public string Email { get; set; }
-        public string Password { get; set; }
+        public string? Password { get; set; }
         public string UserRole { get; set; }
     }
 }
